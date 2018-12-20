5be5b9ca65698ea1cdcf6bf1451d6d53f2b979ae
TABOO\Taboo <tabooxie@163.com>
2018-12-20 21:03:48 +0800

    Update DepartmentController.java

--- microservicecloud-provider-dept-8001/src/main/java/com/taboo/springcloud/controller/DepartmentController.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/taboo/springcloud/controller/DepartmentController.java
@@ -3,7 +3,12 @@ package com.taboo.springcloud.controller;
 import com.taboo.springcloud.entities.Department;
 import com.taboo.springcloud.service.interfaces.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -11,6 +16,9 @@ import java.util.List;
 public class DepartmentController {
     @Autowired
     DepartmentService departmentService;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @RequestMapping(value = "/department/add",method = RequestMethod.POST)
     public boolean add(Department department){
@@ -25,5 +33,11 @@ public class DepartmentController {
     @GetMapping("/department/all")
     public List<Department> list(){
         return departmentService.list();
+    }
+
+    @GetMapping("/department/discovery")
+    public Object discovery() {
+
+        return null;
     }
 }
