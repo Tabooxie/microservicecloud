cbe42990d8b64718208cfc62b5fd002501fec0ba
Tabooxie <tabooxie@163.com>
2019-01-09 21:51:32 +0800

    eureka集群

--- microservicecloud-eureka-7002/src/main/java/com/taboo/springcloud/MicroservicecloudEureka7002Application.java
+++ microservicecloud-eureka-7002/src/main/java/com/taboo/springcloud/MicroservicecloudEureka7002Application.java
@@ -2,8 +2,10 @@ package com.taboo.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MicroservicecloudEureka7002Application {
 
     public static void main(String[] args) {
--- microservicecloud-eureka-7003/src/main/java/com/taboo/springcloud/MicroservicecloudEureka7003Application.java
+++ microservicecloud-eureka-7003/src/main/java/com/taboo/springcloud/MicroservicecloudEureka7003Application.java
@@ -2,8 +2,10 @@ package com.taboo.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MicroservicecloudEureka7003Application {
 
     public static void main(String[] args) {
