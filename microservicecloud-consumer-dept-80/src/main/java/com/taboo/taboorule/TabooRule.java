e43b56043c4e5fc6227d1df2866abb8eeaabcb6a
TABOO\Taboo <tabooxie@163.com>
2019-01-23 18:06:25 +0800

    ribbon DIY

---
+++ microservicecloud-consumer-dept-80/src/main/java/com/taboo/taboorule/TabooRule.java
@@ -0,0 +1,31 @@
+package com.taboo.taboorule;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RoundRobinRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+/**
+ * Copyright: Copyright (c) 2019 Asiainfo
+ * 
+ * @ClassName：com.taboo.taboorule.TabooRule.java
+ * @Description：
+ * 
+ * @version: v1.0.0
+ * @author：xiejun5
+ * @date: 2019-01-23
+ * 
+ * Modification History:
+ * Date           Author            Version            Description
+ *------------------------------------------------------------
+ * 2019-01-23     xiejun5          v1.0.0               修改原因
+ */
+@Configuration
+public class TabooRule {
+
+    @Bean
+    public IRule getDIYRule(){
+        new RoundRobinRule();
+        return new RandomRule();
+    }
+}
