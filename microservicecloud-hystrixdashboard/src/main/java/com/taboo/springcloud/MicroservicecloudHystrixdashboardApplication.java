184c2e188611eda8b857621861185b0513584742
Tabooxie <tabooxie@163.com>
2019-03-11 23:12:08 +0800

    hystrix-dashboard
    增加hystrix-dashboard组件

---
+++ microservicecloud-hystrixdashboard/src/main/java/com/taboo/springcloud/MicroservicecloudHystrixdashboardApplication.java
@@ -0,0 +1,13 @@
+package com.taboo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicecloudHystrixdashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicecloudHystrixdashboardApplication.class, args);
+    }
+
+}
