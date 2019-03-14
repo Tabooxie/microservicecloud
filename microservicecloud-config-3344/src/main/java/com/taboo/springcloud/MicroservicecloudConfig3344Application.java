ab4b67bbb2b80ad17c8e667a0cd21c8587b1fca2
Tabooxie <tabooxie@163.com>
2019-03-14 21:53:17 +0800

    springcloud-config add
    springcloud-config add

---
+++ microservicecloud-config-3344/src/main/java/com/taboo/springcloud/MicroservicecloudConfig3344Application.java
@@ -0,0 +1,13 @@
+package com.taboo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicecloudConfig3344Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicecloudConfig3344Application.class, args);
+    }
+
+}
