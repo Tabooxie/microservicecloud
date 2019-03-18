e912331e5476357f35086341d6e7030fefd2913b
Tabooxie <tabooxie@163.com>
2019-03-19 00:00:19 +0800

    yml

--- microservicecloud-config-client-3355/src/main/java/com/taboo/springcloud/rest/ConfigClientRest.java
+++ microservicecloud-config-client-3355/src/main/java/com/taboo/springcloud/rest/ConfigClientRest.java
@@ -1,8 +1,10 @@
 package com.taboo.springcloud.rest;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ConfigClientRest {
 
     @Value("${spring.application.name}")
@@ -14,7 +16,7 @@ public class ConfigClientRest {
     @Value("${server.port}")
     private String port;
 
-    @RequestMapping("/config")
+    @GetMapping(value = "/config")
     public String getConfig()
     {
         String str = "applicationName: " + applicationName + "\t eurekaServers:" + eurekaServers + "\t port: " + port;
